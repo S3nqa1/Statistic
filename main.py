a3fe4990d20b1444cccece39e737acb77d4ee96a
OlehVoitovych <oleg.voytovych04@gmail.com>
2023-05-29 04:04:54 +0300

    1

--- main.py
+++ main.py
@@ -2,26 +2,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def conditional_means(table, tabley):
+def ni(table):
     ni_list = []
-    yn = []
-    means = []
-
     for i in range(len(table[0])):
         sum = 0
         for j in range(len(table)):
             sum += table[j][i]
         ni_list.append(sum)
+    return ni_list
 
+
+def conditional_means(table, Y, ni_list):
+    yn = []
+    means = []
     for i in range(len(table[0])):
         sum = 0
         for j in range(len(table)):
-            sum += tabley[j] * table[j][i]
+            sum += Y[j] * table[j][i]
         yn.append(sum)
 
     for i in range(len(ni_list)):
         means.append(float("{:.2f}".format(yn[i] / ni_list[i])))
-
     return means
 
 
@@ -65,8 +66,36 @@ def parabolic_corelation(X, Y):
     return coefficients
 
 
-def giperbolic_corelation(X, Y):
+def parabolic_corelation_new(X,meanY,ni):
+    k = len(X)
+    sum_n = 0
+    sum_xn = 0
+    sum_xxn = 0
+    sum_xxxn = 0
+    sum_xxxxn = 0
+    sum_ny = 0
+    sum_xny = 0
+    sum_xxny = 0
 
+    for i in range(len(k)):
+        sum_n += ni[i]
+        sum_xn += X[i]*ni[i]
+        sum_xxn += pow(X[i], 2)*ni[i]
+        sum_xxxn += pow(X[i], 3) * ni[i]
+        sum_xxxxn += pow(X[i], 4) * ni[i]
+        sum_ny += ni[i]*meanY[i]
+        sum_xny += X[i]*ni[i]*meanY[i]
+        sum_xxny += pow(X[i], 2)*ni[i]*meanY[i]
+
+    left_side = np.array([[sum_xxxxn, sum_xxxn, sum_xxn],[sum_xxxn,sum_xxn,sum_xn],[sum_xxn,sum_xn,sum_n]])
+    right_side = np.array([sum_xxny,sum_xny,sum_ny])
+
+    print(np.linalg.inv(left_side).dot(right_side))
+
+
+
+
+def giperbolic_corelation(X, Y):
     X_matrix = np.array([1 / X, np.ones_like(X)]).T
     Y_vector = np.array(Y)
 
@@ -74,6 +103,8 @@ def giperbolic_corelation(X, Y):
     return coefficients
 
 
+correlation_x = [3, 4, 7, 10, 11, 14, 17]
+correlation_y = [1, 2, 2.5, 3, 4, 4.5]
 correlation_table = [
     [18, 0, 0, 0, 0, 0, 0],
     [2, 18, 3, 0, 0, 0, 0],
@@ -82,18 +113,14 @@ correlation_table = [
     [0, 0, 0, 0, 16, 4, 4],
     [0, 0, 0, 0, 0, 22, 3]
 ]
-correlation_x = [3, 4, 7, 10, 11, 14, 17]
-correlation_y = [1, 2, 2.5, 3, 4, 4.5]
-
-correlation_means = conditional_means(correlation_table, correlation_y)
-print(correlation_means)
-
-makePlot(correlation_x, correlation_means)
-
+correlation_ni = ni(correlation_table)
+correlation_means = conditional_means(correlation_table, correlation_y, correlation_ni)
 coeficients = parabolic_corelation(correlation_x, correlation_means)
 
-makeParabol(correlation_x, correlation_means, coeficients)
+parabolic_corelation_new(correlation_x,correlation_means,correlation_ni)
 
+makePlot(correlation_x, correlation_means)
+makeParabol(correlation_x, correlation_means, coeficients)
 
 '''while True:
     option = int(input("enter option == "))
